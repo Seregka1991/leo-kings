13cb50d1b481f26f041d1856caa46f939a622163
Seregka1991 <113276361+Seregka1991@users.noreply.github.com>
2022-09-12 08:54:13 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,166 @@
-const swiper=new Swiper(".swiper-wrapper-gallery",{direction:"horizontal",loop:!0,spaceBetween:20,autoplay:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination",clickable:!0,renderBullet:function(o,t){return'<span class="dot swiper-pagination-bullet"></span>'}},breakpoints:{480:{slidesPerView:1},576:{slidesPerView:2},1330:{slidesPerView:3}}});jQuery(document).ready(function(o){o(window).on("load",function(){return o(".scrolls-main").mCustomScrollbar(),!1})}),jQuery(document).ready(function(o){o(".scrolls-main").mCustomScrollbar({axis:"y",theme:"light-thick",scrollInertia:"430",mouseWheel:{deltaFactor:400}}),jQuery(document).ready(function(o){o(window).on("load",function(){return o(".scrolls-main2").mCustomScrollbar(),!1})}),jQuery(document).ready(function(o){o(".scrolls-main2").mCustomScrollbar({axis:"y",theme:"light-thick",scrollInertia:"430",mouseWheel:{deltaFactor:400}})}),jQuery(document).ready(function(o){o(window).on("load",function(){return o(".scrolls-main3").mCustomScrollbar(),!1})}),jQuery(document).ready(function(o){o(".scrolls-main3").mCustomScrollbar({axis:"y",theme:"light-thick",scrollInertia:"430",mouseWheel:{deltaFactor:400}})})}),jQuery(document).ready(function(o){o(".btn1").click(function(){o("html,body").animate({scrollTop:o(".section-two").offset().top},800)}),o(".btn2").click(function(){o("html,body").animate({scrollTop:o(".section-three").offset().top},800)}),o(".btn3").click(function(){o("html,body").animate({scrollTop:o(".section-four").offset().top},800)}),o(".btn4").click(function(){o("html,body").animate({scrollTop:o(".section-five").offset().top},800)}),o(".scroll-down").click(function(){o("html,body").animate({scrollTop:o(".section-two").offset().top},800)})});
 
+    const swiper = new Swiper(".swiper-wrapper-gallery", {
+      direction: 'horizontal',
+      loop: true,
+      spaceBetween: 20,
+      autoplay: true,
+      navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+          renderBullet: function (index, className) {
+            return `<span class="dot swiper-pagination-bullet"></span>`;
+          },
+        },
+        breakpoints: {
+            480: {
+                slidesPerView: 1,
+                
+            },
+            576: {
+                slidesPerView: 2,
+                
+            },
+            1330: {
+                slidesPerView: 3,
+                
+            },
+         },
+ 
+    });
+    
+
+     jQuery(document).ready(function($){
+        $(window).on("load", function () {
+            $(".scrolls-main").mCustomScrollbar();
+            return false;
+        });
+    });
+
+
+
+    jQuery(document).ready(function($){
+
+    $(".scrolls-main").mCustomScrollbar({
+        axis: "y",              // вертикальный скролл
+        theme: "light-thick",  // тема
+        scrollInertia: "430",   // продолжительность прокрутки, значение в миллисекундах
+        mouseWheel: {
+            deltaFactor: 400    // кол-во пикселей на одну прокрутку колёсика мыши
+        }
+       
+    });
+
+
+    jQuery(document).ready(function($){
+        $(window).on("load", function () {
+            $(".scrolls-main2").mCustomScrollbar();
+            return false;
+        });
+       
+    });
+
+
+
+    jQuery(document).ready(function($){
+
+    $(".scrolls-main2").mCustomScrollbar({
+        axis: "y",              // вертикальный скролл
+        theme: "light-thick",  // тема
+        scrollInertia: "430",   // продолжительность прокрутки, значение в миллисекундах
+
+        mouseWheel: {
+            deltaFactor: 400    // кол-во пикселей на одну прокрутку колёсика мыши
+        }
+        });
+    });
+
+    jQuery(document).ready(function($){
+        $(window).on("load", function () {
+            $(".scrolls-main3").mCustomScrollbar();
+            return false;
+        });
+    });
+
+
+
+    jQuery(document).ready(function($){
+
+    $(".scrolls-main3").mCustomScrollbar({
+        axis: "y",              // вертикальный скролл
+        theme: "light-thick",  // тема
+        scrollInertia: "430",   // продолжительность прокрутки, значение в миллисекундах
+
+        mouseWheel: {
+            deltaFactor: 400    // кол-во пикселей на одну прокрутку колёсика мыши
+        }
+        });
+    });
+
+    
+}); 
+
+
+
+jQuery( document ).ready(function($) {
+
+    $('.btn1').click(function(){
+        $('html,body').animate({scrollTop:$('.section-two').offset().top}, 800);
+    });
+    $('.btn2').click(function(){
+        $('html,body').animate({scrollTop:$('.section-three').offset().top}, 800);
+    });
+     $('.btn3').click(function(){
+        $('html,body').animate({scrollTop:$('.section-four').offset().top}, 800);
+    }); 
+    $('.btn4').click(function(){
+        $('html,body').animate({scrollTop:$('.section-five').offset().top}, 800);
+    });
+    $('.scroll-down').click(function(){
+        $('html,body').animate({scrollTop:$('.section-two').offset().top}, 800);
+    });
+});
+
+
+// Проверяем, можно ли использовать Webp формат
+function canUseWebp() {
+    // Создаем элемент canvas
+    let elem = document.createElement('canvas');
+    // Приводим элемент к булеву типу
+    if (!!(elem.getContext && elem.getContext('2d'))) {
+        // Создаем изображение в формате webp, возвращаем индекс искомого элемента и сразу же проверяем его
+        return elem.toDataURL('image/webp').indexOf('data:image/webp') == 0;
+    }
+    // Иначе Webp не используем
+    return false;
+}
+
+
+window.onload = function () {
+    // Получаем все элементы с дата-атрибутом data-bg
+    let images = document.querySelectorAll('[data-bg]');
+    // Проходимся по каждому
+    for (let i = 0; i < images.length; i++) {
+        // Получаем значение каждого дата-атрибута
+        let image = images[i].getAttribute('data-bg');
+        // Каждому найденному элементу задаем свойство background-image с изображение формата jpg
+        images[i].style.backgroundImage = 'url(' + image + ')';
+    }
+
+    // Проверяем, является ли браузер посетителя сайта Firefox и получаем его версию
+    let isitFirefox = window.navigator.userAgent.match(/Firefox\/([0-9]+)\./);
+    let firefoxVer = isitFirefox ? parseInt(isitFirefox[1]) : 0;
+
+    // Если есть поддержка Webp или браузер Firefox версии больше или равно 65
+    if (canUseWebp() || firefoxVer >= 65) {
+        // Делаем все то же самое что и для jpg, но уже для изображений формата Webp
+        let imagesWebp = document.querySelectorAll('[data-bg-webp]');
+        for (let i = 0; i < imagesWebp.length; i++) {
+            let imageWebp = imagesWebp[i].getAttribute('data-bg-webp');
+            imagesWebp[i].style.backgroundImage = 'url(' + imageWebp + ')';
+        }
+    }
+};
